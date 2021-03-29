2279f152c69ff504d559e20338188eb85ebd0299
lubear <lubear.tw@yahoo.com.tw>
2021-03-29 19:54:26 +0800

    End of Section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,29 +1,50 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        
-        private DataContext _context;
-        public ActivitiesController(DataContext context){
-            _context = context;
-        }
+    
+        // private readonly IMediator _mediator;
+        // public ActivitiesController(IMediator mediator){
+        //     _mediator = mediator;
+        // }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _context.Activities.ToListAsync();
+            //return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id){
-            return await _context.Activities.FindAsync(id);
+            //return await _context.Activities.FindAsync(id);
+            //return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity){
+
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditActivity(Guid id, Activity activity){
+            
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(Guid id){
+            
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
